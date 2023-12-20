d0ca20885d843046153d59a6b3bda3703bdee7c0
Matthis Hagen <matthis.hagen@o2online.de>
2023-12-20 02:58:34 +0100

    Add function to find char positions

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,23 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn find_chars_positions(input: &str, char_test: fn(char) -> bool) -> Vec<(usize, usize)> {
+    input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.char_indices()
+                .filter_map(move |(x, c)| match char_test(c) {
+                    true => Some((x, y)),
+                    false => None,
+                })
+        })
+        .collect::<Vec<(usize, usize)>>()
+}
+
+pub fn manhattan_dist(lhs: &(usize, usize), rhs: &(usize, usize)) -> usize {
+    lhs.0.abs_diff(rhs.0) + lhs.1.abs_diff(rhs.1)
+}
+
+pub fn manhattan_dist_u64(lhs: &(u64, u64), rhs: &(u64, u64)) -> u64 {
+    lhs.0.abs_diff(rhs.0) + lhs.1.abs_diff(rhs.1)
 }
 
 #[cfg(test)]
@@ -7,8 +25,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_find_char_positions() {
+        let result = find_chars_positions(
+            "....#.
+..#...
+#.....",
+            |c| c == '#',
+        );
+        assert_eq!(result, vec![(4, 0), (2, 1), (0, 2)]);
+    }
+
+    #[test]
+    fn test_manhattan_dist() {
+        let result = manhattan_dist(&(1, 3), &(4, 5));
+        assert_eq!(result, 5)
     }
 }
